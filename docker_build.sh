00c08722a17433b7efa596cd35c0471f3decb498
jarek <iaroslav.pavlov@unibas.ch>
2025-05-06 09:01:56 +0200

    preparing for merging

--- docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build --rm -t dlza-manager-storage-handler:latest .
-docker tag dlza-manager-storage-handler:latest registry.localhost:5001/dlza-manager-storage-handler
-docker push registry.localhost:5001/dlza-manager-storage-handler
