86816a152f0c9e26124854774ec0d3993e89abd3
jarek <iaroslav.pavlov@unibas.ch>
2026-03-31 09:15:35 +0200

    Testing some methods

--- service/s3Service.go
+++ service/s3Service.go
@@ -14,27 +14,27 @@ type S3Service struct {
 }
 
 func (s *S3Service) PutObject(ctx context.Context, input *s3.PutObjectInput, opt ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.PutObject(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) ListParts(ctx context.Context, input *s3.ListPartsInput, opt ...func(*s3.Options)) (*s3.ListPartsOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.ListParts(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) UploadPart(ctx context.Context, input *s3.UploadPartInput, opt ...func(*s3.Options)) (*s3.UploadPartOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.UploadPart(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) GetObject(ctx context.Context, input *s3.GetObjectInput, opt ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.GetObject(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) HeadObject(ctx context.Context, input *s3.HeadObjectInput, opt ...func(*s3.Options)) (*s3.HeadObjectOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.HeadObject(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) CreateMultipartUpload(ctx context.Context, input *s3.CreateMultipartUploadInput, opt ...func(*s3.Options)) (*s3.CreateMultipartUploadOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	res, err := s.Client.CreateMultipartUpload(ctx, input, s.AddDisableEndpointPrefix)
 	if err != nil {
 		return nil, fmt.Errorf("couuld not create multipart upload: %w", err)
@@ -42,34 +42,32 @@ func (s *S3Service) CreateMultipartUpload(ctx context.Context, input *s3.CreateM
 	return res, nil
 }
 func (s *S3Service) AbortMultipartUpload(ctx context.Context, input *s3.AbortMultipartUploadInput, opt ...func(*s3.Options)) (*s3.AbortMultipartUploadOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.AbortMultipartUpload(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) DeleteObject(ctx context.Context, input *s3.DeleteObjectInput, opt ...func(*s3.Options)) (*s3.DeleteObjectOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.DeleteObject(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) DeleteObjects(ctx context.Context, input *s3.DeleteObjectsInput, opt ...func(*s3.Options)) (*s3.DeleteObjectsOutput, error) {
 	return s.Client.DeleteObjects(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) CompleteMultipartUpload(ctx context.Context, input *s3.CompleteMultipartUploadInput, opt ...func(*s3.Options)) (*s3.CompleteMultipartUploadOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.CompleteMultipartUpload(ctx, input, s.AddDisableEndpointPrefix)
 }
 func (s *S3Service) UploadPartCopy(ctx context.Context, input *s3.UploadPartCopyInput, opt ...func(*s3.Options)) (*s3.UploadPartCopyOutput, error) {
-	input.Key = trimKey(input.Key)
+	input.Key = TrimKey(input.Key)
 	return s.Client.UploadPartCopy(ctx, input, s.AddDisableEndpointPrefix)
 }
 
-func trimKey(key *string) *string {
-	formattedKeyArray := strings.Split(*key, "-")
+func TrimKey(key *string) *string {
+	splitKeyArray := strings.Split(*key, "/")
+
+	formattedKeyArray := strings.Split(splitKeyArray[len(splitKeyArray)-1], "-")
 	if len(formattedKeyArray) == 1 {
 		return key
 	}
-	prefix := ""
-	if strings.Contains(formattedKeyArray[0], "/") {
-		prefix = strings.Split(formattedKeyArray[0], "/")[0] + "/"
-	}
-	res := prefix + formattedKeyArray[len(formattedKeyArray)-1]
+	res := splitKeyArray[0] + "/" + formattedKeyArray[len(formattedKeyArray)-1]
 	return &res
 }
---
+++ tests/service_test.go
@@ -0,0 +1,39 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/ocfl-archive/dlza-manager-storage-handler/service"
+)
+
+func sptr(s string) *string { return &s }
+
+func TestTrimKey(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		{name: "t1", in: "89280007-8674-4aa8-9b71-8943cfaf96ff-partition-1/tenantname-89280007-8674-4aa8-9b71-8943cfaf96ff-filename.zip", want: "89280007-8674-4aa8-9b71-8943cfaf96ff-partition-1/filename.zip"},
+		{name: "t2", in: "archive/tenantname-89280007-8674-4aa8-9b71-8943cfaf96ff-filename.zip", want: "archive/filename.zip"},
+		{name: "t3", in: "89280007-8674-4aa8-9b71-8943cfaf96ff-partition-1/file/tenantname-89280007-8674-4aa8-9b71-8943cfaf96ff-filename.zip", want: "89280007-8674-4aa8-9b71-8943cfaf96ff-partition-1/filename.zip"},
+		{name: "t4", in: "archive/file/tenantname-89280007-8674-4aa8-9b71-8943cfaf96ff-filename.zip", want: "archive/filename.zip"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := service.TrimKey(sptr(tt.in))
+			if got == nil {
+				t.Fatalf("got nil, want %q", tt.want)
+			}
+			if *got != tt.want {
+				t.Fatalf("got %q, want %q", *got, tt.want)
+			}
+		})
+	}
+}
